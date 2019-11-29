36dc8fb458e561d976e35e662f63af24c35180cc
Ugo Arzur <ugo.arzur@morning.com>
2019-11-29 12:27:31 +0100

    first push

---
+++ src/store/index.js
@@ -0,0 +1,23 @@
+/* eslint-disable no-shadow */
+
+import Vue from 'vue';
+import Vuex from 'vuex';
+
+// import * as helperReports from '../helpers/reports';
+
+Vue.use(Vuex);
+
+const state = {};
+
+const mutations = {};
+
+const actions = {};
+
+const getters = {};
+
+export default new Vuex.Store({
+  state,
+  mutations,
+  actions,
+  getters,
+});
---
+++ src/views/Cheh.vue
@@ -0,0 +1,3 @@
+<template>
+  <div>Cheh</div>
+</template>
